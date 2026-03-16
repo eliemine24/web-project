0f793f8e956bcd5245ff7055436cf1372f71f4a6
Csarramea <camille.sarramea@insa-lyon.fr>
2026-03-16 09:36:45 +0100

    maquette à peu près finie

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+  function handleCount(){
+    setCount(count + 1);
+}
+  
   return (
     <div className="App">
       <header className="App-header">
         <div className="Container">
+          <div className="Compteur">{count} titres</div>
           <h1>discover</h1>
-          <MyButton couleur="tomato" symbole="❤︎" top="400px" right= "20px"/>  
-          <MyButton couleur="blue" symbole="✗" top="400px" right= "220px"/>  
+          <div className="Music-Card">
+            <div className="Music-Info">
+              <h2>Song Title</h2>
+              <p>Artist Name</p>
+            </div>
+          </div>
+          <MyButton couleur="#ff4458" symbole="❤︎" bottom="14%" right="15%" onClick={handleCount}/>  
+          <MyButton couleur="#24292e" symbole="✗" bottom="14%" left="15%"/> 
+          {count > 0 && (<ButtonTerm/>)}
+          
         </div>
       </header>
     </div>
@@ -16,11 +31,30 @@ function App() {
 }
 
 
-function MyButton({couleur, symbole, top, right}) {
+function MyButton({couleur, symbole, bottom, right, left, onClick}) {
   return (
-    <button className="Mon-bouton" style={{ backgroundColor: couleur,  top: top, right: right }}>
+    <button 
+      className="Mon-bouton" 
+      onClick={onClick}
+      style={{ 
+        backgroundColor: couleur,  
+        bottom: bottom, 
+        right: right,
+        left: left 
+      }}
+    >
       {symbole}
     </button>
   );
+}
+
+function ButtonTerm(){
+  return(
+    <button 
+      className="Bouton-Terminer"
+      style={{bottom: '3%'}} 
+    >
+      DONE !
+    </button>)
 }
 export default App;
