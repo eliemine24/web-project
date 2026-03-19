4dad4cd1abecf4e62044af305f1dcd7c1a4238d9
Csarramea <camille.sarramea@insa-lyon.fr>
2026-03-19 10:25:01 +0100

    recos : squelette

--- src/Recos.js
+++ src/Recos.js
@@ -3,3 +3,20 @@
 Algo de recommandation 
 
 */
+
+function Recommandations(){
+    /* demande 6 nouvelles reco via les autres fonctions*/
+    /* une fois les reco obtenues, applique le request maker dessus pour avoir les liens*/ 
+}
+
+function RecoGenre(){
+    /* donne une reco en fonction de si on aime ou non le genre */
+}
+
+function RecoArtiste(){
+    /* donne une reco en fonction de si on aime ou non l'artiste */
+}
+
+function RequestMaker(){
+    
+}
