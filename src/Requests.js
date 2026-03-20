502a2fc2d331f9a228cc10d721cbea940cf3144b
eliemine24 <elie.gautier24@gmail.com>
2026-03-20 14:42:35 +0100

    branch for request making

---
+++ src/Requests.js
@@ -0,0 +1,17 @@
+/* http iTunes api requests maker from "id" and "name" argument */
+
+async function requestMaker(id="", name="") {
+
+    let req = "";
+    if (listeArtistes.has(id)) {
+        // build http request to get a song from the artists "id" from the iTunes api
+        // store it in req
+    }
+
+    if (listeArtistes.has(id)) {
+        // build http request to get a song from the artists "id" from the iTunes api
+        // store it in req
+    }
+
+    return req;
+}
