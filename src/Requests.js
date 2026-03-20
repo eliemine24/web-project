277dc2d0bc4e10ff9f98bbd833004887fbe13251
eliemine24 <elie.gautier24@gmail.com>
2026-03-20 14:52:47 +0100

    request maker finito

--- src/Requests.js
+++ src/Requests.js
@@ -1,17 +1,37 @@
-/* http iTunes api requests maker from "id" and "name" argument */
+/* http iTunes api request maker from "id" and "name" arguments */
 
-async function requestMaker(id="", name="") {
+import { listeArtistes, listeGenres } from "./Model";
 
-    let req = "";
-    if (listeArtistes.has(id)) {
-        // build http request to get a song from the artists "id" from the iTunes api
-        // store it in req
+export async function requestMaker(id = "", name = "") {
+  // Default query when on ne sait pas quoi chercher
+  const defaultTerm = "pop";
+  const defaultGenreId = "14";
+
+  // term: on privilégie le nom fourni, sinon on retient "pop"
+  let term = defaultTerm;
+  if (name && name.trim().length > 0) {
+    term = name.trim();
+  }
+
+  // genreId: par défaut 14 (pop). Si l'id correspond à un genre existant, on l'utilise.
+  let genreId = defaultGenreId;
+  if (id && listeGenres?.has && listeGenres.has(id)) {
+    genreId = id;
+  }
+
+  // Si l'id correspond à un artiste, on peut utiliser spécialement son nom
+  if (id && listeArtistes?.has && listeArtistes.has(id)) {
+    const artiste = listeArtistes.get(id);
+    if (artiste?.nom) {
+      term = artiste.nom;
     }
+  }
 
-    if (listeArtistes.has(id)) {
-        // build http request to get a song from the artists "id" from the iTunes api
-        // store it in req
-    }
+  const url = new URL("https://itunes.apple.com/search");
+  url.searchParams.set("term", term);
+  url.searchParams.set("entity", "song");
+  url.searchParams.set("limit", "50");
+  url.searchParams.set("genreId", genreId);
 
-    return req;
+  return url.toString();
 }
