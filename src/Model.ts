047c0c0e0d5023a8bbf8e09db787dedab75bf7f1
Cara Saulnier <carocesau@gmail.com>
2026-03-20 14:48:03 +0100

    made it so that the first row of the csv would not be read

--- src/Model.ts
+++ src/Model.ts
@@ -24,14 +24,15 @@ interface Genre {
 
 /**
  * 
- * @param genresCsv 
+ * @param genresCsv le fichier qui contient la liste des genres de musique
  */
 export function uploadGenres(genresCsv: string){
   const rows = genresCsv.trim().split("\n");
-  rows.forEach((row) => {
-    const [path, nom, code] = row.split(",");
+  let i =1;
+  for(; i < rows.length; i++){
+    const [path, nom, code] = rows[i].split(",");
     listeGenres.set(code.trim(), {nom: nom.trim(),score: 0});
-  });
+  };
   listeGenres.forEach((genre)=>{
     console.log(genre);
   });
@@ -39,16 +40,16 @@ export function uploadGenres(genresCsv: string){
 
 /**
  * 
- * @param artiste 
- * @param id 
+ * @param artiste un objet artiste
+ * @param id un identifiant pour l'artiste
  */
-export function addArtist(artiste : Artiste, id : string){
+export function addArtist(id : string, artiste : Artiste){
   listeArtistes.set(id, {nom : artiste.nom,score : artiste.score});
 }
 
 /**
  * 
- * @param genreId 
+ * @param genreId un identifiant pour le genre
  * @param value, si la valeur est négative, le score diminue par 1, sinon il augmente par 1
  */
 export function updateGenreScore(genreId : string, value : number){
@@ -65,14 +66,14 @@ export function updateGenreScore(genreId : string, value : number){
 
 /**
  * 
- * @param genreId 
+ * @param artisteId un identifiant pour l'artiste
  * @param value, si la valeur est négative, le score diminue par 1, sinon il augmente par 1
  */
 export function updateArtistScore(artisteId : string, value : number, nomArtiste : string){
   const artiste = listeArtistes.get(artisteId);
   if(!artiste){
     let score = value<0? -1:1;
-    addArtist({nom: nomArtiste,score : score}, artisteId);
+    addArtist(artisteId, {nom: nomArtiste,score : score});
     return;
   }
   let nouveauScore = artiste.score;
