573d22598566c418ca770e592e67cbc81844b4c0
eliemine24 <elie.gautier24@gmail.com>
2026-03-20 14:57:59 +0100

    request maker in recos

--- src/Recos.js
+++ src/Recos.js
@@ -5,6 +5,7 @@ Algo de recommandation
 */
 
 import mapArtistes from "./Model.js"
+import { listeArtistes, listeGenres } from "./Model";
 
 export default function Recommandations(number){
     /* demande 7 nouvelles reco via les autres fonctions*/
@@ -77,8 +78,38 @@ function RecoAleatoire(){
 }
 
 
-function RequestMaker(){
-    /* je renvoie le nom de l'artiste ou le nom du genre */
+async function requestMaker(id = "", name = "") {
+  // Default query when on ne sait pas quoi chercher
+  const defaultTerm = "pop";
+  const defaultGenreId = "14";
+
+  // term: on privilégie le nom fourni, sinon on retient "pop"
+  let term = defaultTerm;
+  if (name && name.trim().length > 0) {
+    term = name.trim();
+  }
+
+  // genreId: par défaut 14 (pop). Si l'id correspond à un genre existant, on l'utilise.
+  let genreId = defaultGenreId;
+  if (id && listeGenres?.has && listeGenres.has(id)) {
+    genreId = id;
+  }
+
+  // Si l'id correspond à un artiste, on peut utiliser spécialement son nom
+  if (id && listeArtistes?.has && listeArtistes.has(id)) {
+    const artiste = listeArtistes.get(id);
+    if (artiste?.nom) {
+      term = artiste.nom;
+    }
+  }
+
+  const url = new URL("https://itunes.apple.com/search");
+  url.searchParams.set("term", term);
+  url.searchParams.set("entity", "song");
+  url.searchParams.set("limit", "50");
+  url.searchParams.set("genreId", genreId);
+
+  return url.toString();
 }
 
 function RetourUtilisateur(like, artist, genre){
