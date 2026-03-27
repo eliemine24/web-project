48a997dae4da2ddcbfb1d1ab1b8bd3e36d8747fe
Artiyel <128458467+Artiyel@users.noreply.github.com>
2026-03-27 14:22:03 +0100

    Ajout bandeaux page initiale

--- src/View.js
+++ src/View.js
@@ -88,8 +88,35 @@ function View() {
 
 function PageInitiale({onStart}){
   return(
-    <div className="Container">
-    <ButtonStart onClick={onStart}/>
+ 
+    <div className="Container" color='#ffffff'>
+        <div class="horizontal-scroll">
+	        <div class="horizontal-scroll-item item-1"></div>
+	        <div class="horizontal-scroll-item item-2"></div>
+          <div class="horizontal-scroll-item item-3"></div>
+          <div class="horizontal-scroll-item item-4"></div>
+          <div class="horizontal-scroll-item item-5"></div>
+          <div class="horizontal-scroll-item item-1"></div>
+          <div class="horizontal-scroll-item item-2"></div>
+          <div class="horizontal-scroll-item item-3"></div>
+          <div class="horizontal-scroll-item item-4"></div>
+          <div class="horizontal-scroll-item item-5"></div>		
+        </div>
+        <div>
+          <ButtonStart onClick={onStart}/>
+        </div>
+        <div class="horizontal-scroll-bis">
+          <div class="horizontal-scroll-item-bis item-6"></div>
+          <div class="horizontal-scroll-item-bis item-7"></div>
+          <div class="horizontal-scroll-item-bis item-8"></div>
+          <div class="horizontal-scroll-item-bis item-9"></div>
+          <div class="horizontal-scroll-item-bis item-10"></div>	
+          <div class="horizontal-scroll-item-bis item-6"></div>
+          <div class="horizontal-scroll-item-bis item-7"></div>
+          <div class="horizontal-scroll-item-bis item-8"></div>
+          <div class="horizontal-scroll-item-bis item-9"></div>
+          <div class="horizontal-scroll-item-bis item-10"></div>	
+        </div>
     </div>
     )
 
