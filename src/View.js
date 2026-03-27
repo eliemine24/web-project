151ec4cce4c3e02b50f62785c75212612be64e6d
Artiyel <128458467+Artiyel@users.noreply.github.com>
2026-03-27 15:12:56 +0100

    Réorganisation

--- src/View.js
+++ src/View.js
@@ -1,5 +1,6 @@
 
 import './View.css';
+import {PageInitiale} from './PageInitiale.jsx';
 import React, { useState, useEffect } from 'react';
 import Recommandations from './Recos';
 import { addMusic, playlist } from './Model.tsx';
@@ -86,44 +87,7 @@ function View() {
   );
 }
 
-function PageInitiale({onStart}){
-  return(
- 
-    <div className="Container" color='#ffffff'>
-        <div class="horizontal-scroll">
-	        <div class="horizontal-scroll-item item-1"></div>
-	        <div class="horizontal-scroll-item item-2"></div>
-          <div class="horizontal-scroll-item item-3"></div>
-          <div class="horizontal-scroll-item item-4"></div>
-          <div class="horizontal-scroll-item item-5"></div>
-          <div class="horizontal-scroll-item item-1"></div>
-          <div class="horizontal-scroll-item item-2"></div>
-          <div class="horizontal-scroll-item item-3"></div>
-          <div class="horizontal-scroll-item item-4"></div>
-          <div class="horizontal-scroll-item item-5"></div>		
-        </div>
-        <div class="Title">
-          Munder
-        </div>
-        <div>
-          <ButtonStart onClick={onStart}/>
-        </div>
-        <div class="horizontal-scroll-bis">
-          <div class="horizontal-scroll-item-bis item-6"></div>
-          <div class="horizontal-scroll-item-bis item-7"></div>
-          <div class="horizontal-scroll-item-bis item-8"></div>
-          <div class="horizontal-scroll-item-bis item-9"></div>
-          <div class="horizontal-scroll-item-bis item-10"></div>	
-          <div class="horizontal-scroll-item-bis item-6"></div>
-          <div class="horizontal-scroll-item-bis item-7"></div>
-          <div class="horizontal-scroll-item-bis item-8"></div>
-          <div class="horizontal-scroll-item-bis item-9"></div>
-          <div class="horizontal-scroll-item-bis item-10"></div>	
-        </div>
-    </div>
-    )
 
-}
 
 function PagePrincipale({count, handleAction, onFinish, url}){
   const [track, setTrack] = useState(null);
@@ -215,14 +179,5 @@ function ButtonTerm({onClick}){
     </button>)
 }
 
-function ButtonStart({onClick}){
-  return(
-    <button 
-      className="Bouton-Start"
-      style={{bottom: '50%'}} 
-      onClick={onClick}
-    >
-      START!
-    </button>)
-}
+
 export default View;
