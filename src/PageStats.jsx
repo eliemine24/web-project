0ea82e442079ce90a561f3fe05b5304aefd84aee
Csarramea <camille.sarramea@insa-lyon.fr>
2026-03-28 14:09:15 +0100

    changement logo bouton

--- src/PageStats.jsx
+++ src/PageStats.jsx
@@ -5,7 +5,7 @@ export function PageStats({onFinish}){
   return(
  
     <div className="Container" color='#ffffff'>
-        <MyButton couleur="#24292e" symbole="⟳" top = "8%" left="5%" onClick={onFinish}/>
+        <MyButton couleur="#24292e" symbole=" ←" top = "8%" left="5%" onClick={onFinish}/>
     </div>
     )
 
